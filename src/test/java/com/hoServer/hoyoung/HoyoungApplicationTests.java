ce6a74a95de7f39e4e1a52e008211110f8b78e59
HoYoungChun <ho723@naver.com>
2022-10-23 13:06:53 +0900

    git init

---
+++ src/test/java/com/hoServer/hoyoung/HoyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoServer.hoyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoyoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
