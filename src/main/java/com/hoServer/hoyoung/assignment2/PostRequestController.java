c1d278bfe2ea0c34eaa9dc7aeb095571704897b4
HoYoungChun <ho723@naver.com>
2022-10-27 19:32:31 +0900

    Add database logic into apis

--- src/main/java/com/hoServer/hoyoung/assignment2/PostRequestController.java
+++ src/main/java/com/hoServer/hoyoung/assignment2/PostRequestController.java
@@ -1,46 +1,64 @@
 package com.hoServer.hoyoung.assignment2;
 
+import com.hoServer.hoyoung.study2.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+
+// 테스트용: INSERT INTO POST VALUES(1, 'Hello' ,'test');
 
 @RestController
 @RequestMapping(value = "posts")
+@RequiredArgsConstructor
 public class PostRequestController {
 
+    private final EntityManager em;
 
     // GET /BASE_URL/posts
     @GetMapping()
-    public String readPostList(){
-        return "post 목록 반환";
+    @Transactional
+    public List<Post> readPostList(){
+        return em.createQuery("SELECT p FROM Post p",Post.class).getResultList();
     }
 
     // POST /BASE_URL/posts
     @PostMapping()
+    @Transactional
     public String createPost(@RequestBody final Post post){
         return "post 생성";
     }
 
     // GET /BASE_URL/posts/{postId}
     @GetMapping("{postId}")
-    public String readOnePost(@PathVariable(value="postId") final String postId){
-        return "id: "+postId+"인 Post 정보 반환";
+    @Transactional
+    public Post readOnePost(@PathVariable(value="postId") final Long postId){
+        return em.find(Post.class, postId);
     }
 
     // PUT /BASE_URL/posts/{postID}
     @PutMapping("{postId}")
-    public String PutOnePost(@PathVariable(value="postId") final String postId){
+    @Transactional
+    public String PutOnePost(@PathVariable(value="postId") final Long postId){
         return "id: "+postId+"인 Post 정보 통으로 교체";
     }
 
     // PATCH /BASE_URL/posts/{postID}
     @PatchMapping("{postId}")
-    public String PatchOnePost(@PathVariable(value="postId") final String postId){
+    @Transactional
+    public String PatchOnePost(@PathVariable(value="postId") final Long postId){
         return "id: "+postId+"인 Post 정보 통으로 일부 변경";
     }
 
     // DELETE /BASE_URL/posts/{postID}
     @DeleteMapping("{postId}")
-    public String DeleteOnePost(@PathVariable(value="postId") final String postId){
-        return "id: "+postId+"인 Post 정보 삭제";
+    @Transactional
+    public void DeleteOnePost(@PathVariable(value="postId") final Long postId){
+        Post post = em.find(Post.class, postId);
+        em.remove(post);
     }
 
 }
